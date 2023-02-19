50b8b94e0fe9b2ac1d11abf74b26bb69208931b2
Williamhiu <125462687+Williamhiu@users.noreply.github.com>
2023-02-19 18:12:15 +0700

    Add files via upload

---
+++ yaudh.py
@@ -0,0 +1,13 @@
+import random
+import os
+import socket
+import threading
+
+os.system("clear")
+print ("AKU SAMUEL FELIX")
+print ("HEY KAMU,YA KAMU,SAYA INGIN BILANG SAYA JOMBLO")
+print ("SAYA SANGAT BOSAN KARNA SAYA JOMBLO BRUH")
+print ("KARNA SAYA JELEK :( FEK SMAIL")
+print ("TUTOR GANTENG ANJING :v !!!!......")
+print ("CITA CITAKU JADI CHEF!")
+BirthdayLate = str(input(" BirtdayLate: "))
